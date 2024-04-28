bbe0f131cd1131ccd209ae3e472598d924e8bf48
Shioiri-Kukuri <1615592660@qq.com>
2024-04-28 15:45:33 +0800

    0.0.0版本初构建

---
+++ gymnasium_parent/src/main/java/com/vky/GymnasiumParentApplication.java
@@ -0,0 +1,13 @@
+package com.vky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymnasiumParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymnasiumParentApplication.class, args);
+    }
+
+}
---
+++ gymnasium_parent/src/test/java/com/vky/GymnasiumParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymnasiumParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
