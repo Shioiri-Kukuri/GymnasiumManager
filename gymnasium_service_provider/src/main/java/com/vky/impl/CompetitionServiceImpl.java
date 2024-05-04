daae83f9e53ec9fd688e06210b8d6408bedc5b89
86188 <202111701136@stu.gdou.edu.cn>
2024-05-04 12:48:45 +0800

    初步架构4.0(修改了pom文件中版本的错误)

--- gymnasium_service_provider/src/main/java/com/vky/impl/CompetitionServiceImpl.java
+++ gymnasium_service_provider/src/main/java/com/vky/impl/CompetitionServiceImpl.java
@@ -1,16 +1,17 @@
 package com.vky.impl;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.annotation.Service;
 import com.vky.dao.CompetitionDao;
 import com.vky.service.CompetitionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
+@Service(interfaceClass = CompetitionService.class)
 @Transactional
 public class CompetitionServiceImpl {
 
-    @Reference
+    @Autowired
     private CompetitionDao competitionDao;
 }
--- gymnasium_service_provider/src/test/java/com/vky/GymnasiumServiceProviderApplicationTests.java
+++ gymnasium_service_provider/src/test/java/com/vky/GymnasiumServiceProviderApplicationTests.java
@@ -3,7 +3,7 @@ package com.vky;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class GymnasiumServiceProviderApplicationTests {
 
     @Test
