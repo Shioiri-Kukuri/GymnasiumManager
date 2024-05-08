285d1db28a4a138d91e2bf3079f84e55fce21fd4
Shioiri-Kukuri <1615592660@qq.com>
2024-05-08 14:25:47 +0800

    Modified some front-end properties. Delete some build-in testing file in a bid to figure out compiler error.

--- src/main/java/com.vky/GymnasiumManagerApplication.java
+++ src/main/java/com.vky/GymnasiumManagerApplication.java
@@ -1,13 +1,10 @@
 package com.vky;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class GymnasiumManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GymnasiumManagerApplication.class, args);
+
     }
 
 }
