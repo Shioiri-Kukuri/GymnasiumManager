c1e3bffc9bdaac6071cf564dc41434679d6e0a5f
Shioiri-Kukuri <1615592660@qq.com>
2024-05-09 17:42:05 +0800

    A working version

--- gymnasium_backend/src/main/java/com/vky/GymnasiumBackendApplication.java
+++ gymnasium_backend/src/main/java/com/vky/GymnasiumBackendApplication.java
@@ -3,9 +3,11 @@ package com.vky;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
 @EnableDubbo
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class GymnasiumBackendApplication {
 
     public static void main(String[] args) {
