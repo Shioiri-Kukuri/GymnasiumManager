83c4e19562bbabd805fca638e58bdeb6131f8298
Shioiri-Kukuri <1615592660@qq.com>
2024-05-16 12:23:55 +0800

    Customized menu

--- gymnasium_backend/src/main/java/com/vky/controller/UserController.java
+++ gymnasium_backend/src/main/java/com/vky/controller/UserController.java
@@ -157,6 +157,32 @@ public class UserController {
         }
         return new Result(true,"删除成功");
     }
+
+    @RequestMapping("/info.do")
+    public Result info(@RequestHeader("Authorization") String token) {
+        try {
+            // 在尝试使令牌失效之前，先验证其有效性。
+            // 这一步确保我们在继续操作前使用的是一个有效的令牌。
+            if (JwtUtil.validateToken(token)) {
+                // 假设JwtUtil中有一个方法来处理令牌失效的逻辑，
+                // 这可能涉及到将令牌添加到黑名单中或管理其有效状态。
+                // 注意：由于JWT无状态的特性，实际的JWT失效处理比较复杂；
+                // 通常，你需要在服务器端维护一个令牌黑名单。
+                boolean isLogoutSuccess = JwtUtil.invalidateToken(token);
+                if (isLogoutSuccess) {
+                    // 通知客户端登出成功。
+                    return new Result(true, "登出成功");
+                } else {
+                    // 如果令牌未能失效，则返回相应的错误信息。
+                    return new Result(false, "登出失败，无法使令牌失效");
+                }
+
+            }
+        }catch (Exception e){
+            return new Result(false,"获取信息失败");
+        }
+        return new Result(true,"获取信息成功");
+    }
 }
 
 
